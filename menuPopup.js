6158dddcb327d1700ec308df923c49e41bb33326
YuriShido <sydonce57@gmail.com>
2021-02-11 16:50:42 -0800

    popMenu fixed

--- menuPopup.js
+++ menuPopup.js
@@ -2,7 +2,7 @@ const popRamenC = document.querySelectorAll('.popRamenC');
 const popRamenOuter = document.querySelectorAll('.popRamenOuter');
 const popAppC  = document.querySelectorAll('.pop-appetizer-c');
 const popDrink = document.querySelectorAll('.pop-drink-c');
-const popOthers = document.querySelectorAll('.popup-others-c');
+const popOthers = document.querySelectorAll('.pop-others-c');
 
 let menuData = {};
 $.ajax({
@@ -15,62 +15,88 @@ $.ajax({
 });
 
 console.log(menuData.ramens)
-
 //get menu from json
+// function getRamenMenu() {
+
+//   for(let i = 0; i < menuData.ramens.length; i++) {
+
+//     const popBox = document.createElement('div');
+//     popBox.classList.add('pop-ramen-c', 'pop-box', 'popRamenC')
+//     const popRamenC = document.querySelectorAll('.popRamenC');
+// // console.log("this is" + popRamenC[i]);
+
+//     console.log(popBox)
+//     console.log(popRamenC)
+//     popRamenC[i].innerHTML = `
+//     <img src="img/ramen${i+1}.jpg">
+//     <div class="menu-detail ">
+//         <h3>${menuData.ramens[i].name}</h3>
+//         <p>${menuData.ramens[i].price}</p>
+//     </div>
+//     <div class="menu-detail">
+//         <p>CA$${menuData.ramens[i].description}</p>
+//         <p>450kcal</p>
+//     </div>
+//     `
+//     popRamenC[i].appendChild(popRamenOuter);
+//   }
+// }
+
 function getRamenMenu() {
 
   for(let i = 0; i < menuData.ramens.length; i++) {
 
-    const popBox = document.createElement('div');
-    popBox.classList.add('pop-ramen-c', 'pop-box', 'popRamenC')
-    const popRamenC = document.querySelector('.popRamenC');
-
-    popRamenC.innerHTML = 
-    `<img src="img/ramen${i+1}.jpg">
+    // console.log(popBox)
+    console.log(popRamenC)
+    popRamenC[i].innerHTML = `
+    <img src="img/ramen${i+1}.jpg">
     <div class="menu-detail ">
         <h3>${menuData.ramens[i].name}</h3>
-        <p>${menuData.ramens[i].price}</p>
+        <p>CA$${menuData.ramens[i].price}</p>
     </div>
     <div class="menu-detail">
-        <p>CA$${menuData.ramens[i].description}</p>
-        <p>450kcal</p>
-    </div>`
-    popRamenC.appendChild(popRamenOuter);
+        <p>${menuData.ramens[i].description}</p>
+        
+    </div>
+    `
+  
   }
 }
 
+
+// console.log(menuData.appetizers.length);
 function getAppetizer() {
    
-  for(let i = 0; i < manuData.appetizers.length; i++) {
-
+  for(let i = 0; i < menuData.appetizers.length; i++) {
+    
      popAppC[i].innerHTML = `
      <img src="img/appetizer${i+1}.jpg">
      <div class="menu-detail ">
          <h3>${menuData.appetizers[i].name}</h3>
-         <p>${menuData.appetizers[i].price}</p>
+         <p>CA$${menuData.appetizers[i].price}</p>
      </div>
      <div class="menu-detail">
-         <p>CA$${menuData.appetizers.description}</p>
-         <p>450kcal</p>
+         <p>${menuData.appetizers[i].description}</p>
+         
      </div>
      `
   }
 
 }
-
+console.log(menuData.drinks.length)
 function getDrinkMenu() {
 
-  for(let i = 0; i < manuData.drinks.length; i++) {
+  for(let i = 0; i < menuData.drinks.length; i++) {
 
      popDrink[i].innerHTML = `
             <img src="img/drink${i + 1}.jpg">
             <div class="menu-detail drink-detail">
-                <h3>${menuData.drinks.name}</h3>
-                <p>${menuData.drinks.price}</p>
+                <h3>${menuData.drinks[i].name}</h3>
+                <p>CA$${menuData.drinks[i].price}</p>
             </div>
             <div class="menu-detail drink-detail">
-                <p>${menuData.drinks.description}</p>
-                <p>450kcal</p>
+                <p>${menuData.drinks[i].description}</p>
+                
             </div>
      `
   }
@@ -79,23 +105,24 @@ function getDrinkMenu() {
 
 function getOtherMenu() {
 
-  for(let i = 0; i < manuData.others.length; i++) {
+  for(let i = 0; i < menuData.others.length; i++) {
       
+    console.log(popOthers[i])
     popOthers[i].innerHTML = `
     <img src="img/other${i+1}.jpg">
     <div class="menu-detail ">
-        <h3>${menuData.others.name}</h3>
-        <p>${menuData.others.price}</p>
+        <h3>${menuData.others[i].name}</h3>
+        <p>CA$${menuData.others[i].price}</p>
     </div>
     <div class="menu-detail">
-        <p>${menuData.others.description}</p>
-        <p>450kcal</p>
+        <p>${menuData.others[i].description}</p>
+       
     </div>
     `
   }
 }
 
-// getRamenMenu();
+getRamenMenu();
 getAppetizer();
-// getDrinkMenu();
+getDrinkMenu();
 getOtherMenu();
